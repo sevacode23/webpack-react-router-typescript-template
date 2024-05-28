c36947a20ebbc5e48fe3ca744e614d311bef95a7
Vsevolod Zoria <vsevolodzoria2@gmail.com>
2024-05-28 16:38:36 +0300

    add-image

--- src/screens/main/main.tsx
+++ src/screens/main/main.tsx
@@ -1,10 +1,14 @@
 import { Link, Outlet } from 'react-router-dom';
 
+import image from '@/assets/images/react-1.webp';
+
 import classes from './main.module.scss';
 
 export const Main = () => (
   <div className={classes.root}>
     <h1>Main Content</h1>
+
+    <img src={image} alt="react" />
 
     <div className={classes.links}>
       <Link to="/admin">Admin</Link>
